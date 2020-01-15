0ed2ce611ab201938ed23c538f740de313c565ca
kin-au <kinnamau@gmail.com>
2020-01-15 16:18:51 +0000

    create landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './LandingPage.css';
+// import fetchPlayer from '../utils/fetchPlayer';
+
+const LandingPage = () => {
+  const [username, setUsername] = React.useState('kin-au');
+  const updateUsername = event => {
+    setUsername(event.target.value);
+  };
+  return (
+    <main>
+      <h1>THE GAME NAME</h1>
+      <form>
+        <label htmlFor='newPlayer'>New Player</label>
+        <input
+          type='text'
+          id='newPlayer'
+          placeholder='Your Github username'
+          value={username}
+          onChange={event => updateUsername(event)}
+        />
+        <button>Go!</button>
+      </form>
+    </main>
+  );
+};
+
+export default LandingPage;
